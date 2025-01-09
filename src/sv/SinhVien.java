f170bf7a2e9ca355ef60f16470e84ba70aac9641
Kiennguyen2406 <155146504+Kiennguyen2406@users.noreply.github.com>
2025-01-09 10:46:56 +0700

    kk

---
+++ src/sv/SinhVien.java
@@ -0,0 +1,37 @@
+package sv;
+
+public class SinhVien {
+    private String hoTen;
+    private String ngaySinh;
+    private String queQuan;
+
+    public SinhVien(String hoTen, String ngaySinh, String queQuan) {
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.queQuan = queQuan;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+}

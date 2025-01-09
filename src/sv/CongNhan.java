262d10b42d81869793c59c1fb84768ccc8c40410
Kiennguyen2406 <155146504+Kiennguyen2406@users.noreply.github.com>
2025-01-09 10:53:49 +0700

    test
    grg

---
+++ src/sv/CongNhan.java
@@ -0,0 +1,36 @@
+package sv;
+
+public class CongNhan {
+    private String hoTen;
+    private String ngaySinh;
+    private String queQuan;
+    public CongNhan(String hoTen, String ngaySinh, String queQuan) {
+        this.hoTen = hoTen;
+        this.ngaySinh = ngaySinh;
+        this.queQuan = queQuan;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public String getQueQuan() {
+        return queQuan;
+    }
+
+    public void setQueQuan(String queQuan) {
+        this.queQuan = queQuan;
+    }
+}
